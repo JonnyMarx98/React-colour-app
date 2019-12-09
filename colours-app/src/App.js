387afd108b9aa40bd511ec50f1ce2a958d0c5854
Jonny <jonnymarx98@gmail.com>
2019-12-09 17:57:59 +0000

    yes

---
+++ colours-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[5]}/>
+    </div>
+  );
+}
+
+export default App;
