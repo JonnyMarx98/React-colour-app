67cc6cc44889f768737fe590f1966668cddc80b8
Jonny <jonnymarx98@gmail.com>
2020-01-12 09:43:10 +0000

    palette route

--- colours-app/src/App.js
+++ colours-app/src/App.js
@@ -7,11 +7,21 @@ import {generatePalette} from './colorHelpers';
 // import './App.css';
 
 class App extends Component{
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    })
+  }
+
   render() {
   return (
     <Switch>
       <Route exact path="/" render={() => <h1>Palette List Goes Here</h1>} />
-      <Route exact path='/palette/:id' render={() => <h1>Individual Palette Goes Here</h1>} />
+      <Route
+        exact
+        path='/palette/:id'
+        render={routeProps => <Palette 
+          palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
     </Switch>
     
     // <div className="App">
