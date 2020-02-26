fbb2e8ee96ae462fcbbd03f587f1c4119d8164ae
Jonny <jonnymarx98@gmail.com>
2020-02-26 18:02:44 +0000

    added delete transition

--- colours-app/src/PaletteList.js
+++ colours-app/src/PaletteList.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import styles from './styles/PaletteListStyles'
+import styles from './styles/PaletteListStyles';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 
 class PaletteList extends Component {
   constructor(props) {
@@ -30,17 +32,25 @@ class PaletteList extends Component {
           <h1 className={classes.heading}>REACT COLORS</h1>
           <Link to="/palette/new">Create New Palette</Link>
           </nav>
-          <div className={classes.palettes}>
-            {palettes.map(palette => (
-              <MiniPalette
-                {...palette}
-                handleClick={() => this.goToPalette(palette.id)}
-                handleDelete={deletePalette}
-                key={palette.id}
-                id={palette.id}  
-              />
-            ))}
-          </div>
+          
+            <TransitionGroup className={classes.palettes}>
+              {palettes.map(palette => (
+                <CSSTransition
+                  key={palette.id}
+                  classNames="fade"
+                  timeout={500}
+                >
+                  <MiniPalette
+                    {...palette}
+                    handleClick={() => this.goToPalette(palette.id)}
+                    handleDelete={deletePalette}
+                    key={palette.id}
+                    id={palette.id}  
+                  />
+                </CSSTransition>
+              ))}
+            </TransitionGroup>
+          
         </div>
         
       </div>
